f10dbb01cb13e26a97f93513d0ff5537c0954049
seid1zahidov <saidiz@code.edu.az>
2022-04-03 10:22:01 -0700

    homejop

---
+++ Program.cs
@@ -0,0 +1,478 @@
+﻿using System;
+
+namespace homewok22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //birinci();
+
+            #region task 2
+
+            //static void birinci()
+            //{
+            //    
+
+            //    try
+            //    {
+            //        Console.WriteLine(" 1 cieded daxil eddin");
+            //        string a = Console.ReadLine();
+            //        int say1 = int.Parse(a);
+
+
+            //        Console.WriteLine(" 2 cieded daxil eddin");
+            //        string b = Console.ReadLine();
+            //        int say2 = int.Parse(b);
+
+            //        Console.WriteLine(" 3 cueded daxil eddin");
+            //        string c = Console.ReadLine();
+            //        int say3 = int.Parse(c);
+
+            //        Console.WriteLine(" 4 cueded daxil eddin");
+            //        string d = Console.ReadLine();
+            //        int say4 = int.Parse(d);
+
+            //        Console.WriteLine(" 5 ci eded daxil eddin");
+            //        string e = Console.ReadLine();
+            //        int say5 = int.Parse(e);
+
+
+            //        double k = (double)(say1 + say2 + say3 + say4 + say5) / 5;
+            //        Console.WriteLine(k);
+
+
+            //    }
+            //    catch (FormatException)
+            //    {
+            //        Console.WriteLine("DUZGUN DAXIL ET");
+
+            //    }
+
+            #endregion
+
+            #region homewrok1
+
+            //{
+
+            //    int a = 5;
+            //    int b = 6;
+            //    int c = 7;
+
+            //    if (a > b && a > c)
+            //    {
+            //        Console.WriteLine($"en boyuk  = {a}");
+
+            //    }
+            //    else if (b > a && b > c)
+            //    {
+            //        Console.WriteLine($"en boyuk  = {b}");
+
+            //    }
+            //    else if (c > b && c > a)
+            //    {
+            //        Console.WriteLine($"en boyuk  = {c}");
+
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("beraberlik var ");
+            //    }
+
+
+            //}
+
+            #endregion
+            //perimetr();
+            #region task 3
+
+            //static void perimetr()
+            //{
+
+
+            //    try
+            //    {
+            //        Console.WriteLine("enini daxil edin");
+            //        string a = Console.ReadLine();
+            //        int en = int.Parse(a);
+
+            //        Console.WriteLine("uzunlugunu daxil et");
+            //        string b = Console.ReadLine();
+            //        int uz = int.Parse(b);
+
+            //        int s = (en * uz);
+            //        Console.WriteLine($"sahesi {a}");
+
+            //        int p = (en + uz) * 2;
+            //        Console.WriteLine($"perimetri {b}");
+            //    }
+            //    catch (FormatException)
+            //    {
+            //        Console.WriteLine("ededi duzgun daxil ele");
+
+            //    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            #endregion
+            //sira();
+            #region task 4
+            //static void sira()
+            //{
+            //    Console.WriteLine("eded daxil edin");
+
+
+
+
+            //    try
+            //    {
+            //        string a = Console.ReadLine();
+            //        int sira = int.Parse(a);
+
+            //        int count = 0;
+
+            //        if (sira > 1)
+            //        {
+
+
+            //            for (int i = 0; i < sira; i++)
+            //            {
+            //                if (count > i)
+            //                {
+            //                    count++;
+
+            //                    Console.WriteLine("eded sadedir");
+
+
+            //                    if (count < 2)
+            //                    {
+            //                        Console.WriteLine("eded murrrekebdir");
+
+            //                    }
+
+
+            //                }
+            //                if (count <= 2)
+            //                {
+            //                    Console.WriteLine("murrekebdir");
+            //                    break;
+            //                }
+            //            }
+
+
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("eded 1 den balaca yada 1 e beraber ola bilmez");
+
+            //        }
+
+
+
+
+
+            //    }
+            //    catch (FormatException)
+            //    {
+            //        Console.WriteLine("duzgun daxil et");
+
+            //    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //}
+            #endregion
+
+
+            //sira();
+            #region task 5
+
+            //static void sira()
+            //{
+
+
+            //    try
+            //    {
+
+            //        string a = Console.ReadLine();
+            //        int mr = int.Parse(a);
+            //        int count = 0;
+
+            //        while (mr > 0)
+            //        {
+            //            mr = mr / 10;
+            //            count++;
+            //        }
+            //        Console.WriteLine($"merbesi var {count}");
+
+            //    }
+            //    catch (FormatException)
+            //    {
+            //        Console.WriteLine("duzgun daxil et sadece reqemlerden ibaret olmalidir");
+
+
+            //    }
+            #endregion
+            //seid();
+            #region task 6
+
+
+            //static void seid()
+
+
+
+            //{
+
+            //    try
+            //    {
+            //        Console.WriteLine("eded daxil edin");
+            //        string a = Console.ReadLine();
+            //        int nt = int.Parse(a);
+
+            //        while (nt > 1 && nt % 21 == 0)
+            //        {
+            //            Console.WriteLine("hem 3 e hemde 7 e bolunur");
+            //            break;
+
+            //        }
+
+            //    }
+            //    catch (FormatException)
+            //    {
+            //        Console.WriteLine("ededi duzgun daxil et");
+
+
+            //    }
+
+
+
+
+
+
+            //}
+            #endregion
+            //bal();
+            #region task7
+
+
+            //static void bal()
+            //{
+
+            //    try
+            //    {
+            //        Console.WriteLine("eded daxil eliyin");
+            //        string a = Console.ReadLine();
+            //        int bal = int.Parse(a);
+
+            //        if (bal > 0 && bal < 100)
+            //        {
+
+
+            //            if (bal > 91 && bal <= 100)
+            //            {
+
+            //                Console.WriteLine("neticeniz A");
+
+            //            }
+            //            else if (bal > 81 && bal <= 90)
+            //            {
+            //                Console.WriteLine("neticeniz b");
+
+            //            }
+            //            else if (bal > 71 && bal <= 80)
+            //            {
+            //                Console.WriteLine("neticeniz c");
+
+            //            }
+            //            else if (bal > 61 && bal <= 70)
+            //            {
+            //                Console.WriteLine("neticeniz d");
+
+
+            //            }
+            //            else if (bal > 51 && bal <= 60)
+            //            {
+            //                Console.WriteLine("neticeniz e");
+
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("kesildin");
+            //            }
+
+
+            //    catch (FormatException)
+            //    {
+            //        Console.WriteLine("ededi duzgun daxil et");
+
+
+            //    }
+
+
+
+
+
+
+
+            //}
+
+
+
+
+
+
+
+        }
+        #endregion
+    }
+    
+} 
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
